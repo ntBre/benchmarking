85de2d07e74de9374861bbc41f74dd76568e4724
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-10 11:29:25 -0400

    fmt

--- sage/02-b-minimize.py
+++ sage/02-b-minimize.py
@@ -15,8 +15,8 @@ from simtk.openmm.app import ForceField
 
 def run_openmm(molecule: Molecule, system: openmm.System):
     """
-    Minimize molecule with specified system and return the positions of the optimized
-    molecule.
+    Minimize molecule with the specified system and return the optimized
+    positions.
     """
 
     # Make sure we consistently only use OE in this script
@@ -30,11 +30,15 @@ def run_openmm(molecule: Molecule, system: openmm.System):
     platform = openmm.Platform.getPlatformByName("Reference")
 
     openmm_context = openmm.Context(system, integrator, platform)
-    openmm_context.setPositions(molecule.conformers[0].value_in_unit(unit.nanometer))
+    openmm_context.setPositions(
+        molecule.conformers[0].value_in_unit(unit.nanometer)
+    )
 
     openmm.LocalEnergyMinimizer.minimize(openmm_context, 5.0e-9, 1500)
 
-    conformer = openmm_context.getState(getPositions=True).getPositions(asNumpy=True)
+    conformer = openmm_context.getState(getPositions=True).getPositions(
+        asNumpy=True
+    )
     energy = openmm_context.getState(getEnergy=True).getPotentialEnergy()
 
     return conformer, energy.value_in_unit(unit.kilocalories_per_mole)
@@ -58,19 +62,19 @@ def run_openmm(molecule: Molecule, system: openmm.System):
     type=click.Path(exists=False, dir_okay=False),
 )
 @click.option(
-    "-o", "--output", "output_path", type=click.Path(exists=False, dir_okay=False)
+    "-o",
+    "--output",
+    "output_path",
+    type=click.Path(exists=False, dir_okay=False),
 )
 def main(input_path, force_field_path, force_field_type, output_path):
-
     input_stream = oechem.oemolistream(input_path)
     output_stream = oechem.oemolostream(output_path)
 
     failed = False
 
     try:
-
         for oe_molecule in input_stream.GetOEGraphMols():
-
             oe_molecule = oechem.OEGraphMol(oe_molecule)
             oechem.OE3DToInternalStereo(oe_molecule)
 
@@ -85,26 +89,32 @@ def main(input_path, force_field_path, force_field_type, output_path):
 
             off_molecule._conformers = [
                 numpy.array(
-                    [oe_molecule.GetCoords()[i] for i in range(off_molecule.n_atoms)]
+                    [
+                        oe_molecule.GetCoords()[i]
+                        for i in range(off_molecule.n_atoms)
+                    ]
                 )
                 * unit.angstrom
             ]
 
             if force_field_type.lower() == "smirnoff":
-
                 smirnoff_force_field = smirnoff.ForceField(
                     force_field_path, allow_cosmetic_attributes=True
                 )
 
-                if "Constraints" in smirnoff_force_field.registered_parameter_handlers:
-                    smirnoff_force_field.deregister_parameter_handler("Constraints")
+                if (
+                    "Constraints"
+                    in smirnoff_force_field.registered_parameter_handlers
+                ):
+                    smirnoff_force_field.deregister_parameter_handler(
+                        "Constraints"
+                    )
 
                 omm_system = smirnoff_force_field.create_openmm_system(
                     off_molecule.to_topology()
                 )
 
             elif force_field_type.lower() == "gaff":
-
                 force_field = ForceField()
 
                 generator = GAFFTemplateGenerator(
@@ -120,7 +130,6 @@ def main(input_path, force_field_path, force_field_type, output_path):
                 )
 
             else:
-
                 raise NotImplementedError()
 
             new_conformer, energy = run_openmm(off_molecule, omm_system)
@@ -136,8 +145,9 @@ def main(input_path, force_field_path, force_field_type, output_path):
             oechem.OEWriteMolecule(output_stream, oe_molecule)
 
     except BaseException:
-
-        logging.exception(f"failed to minimize {input_path} with {force_field_path}")
+        logging.exception(
+            f"failed to minimize {input_path} with {force_field_path}"
+        )
         failed = True
 
     input_stream.close()
