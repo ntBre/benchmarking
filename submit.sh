471270bf11acba4520bc313c56224f4665d68329
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-14 09:54:36 -0400

    add submit.sh for running make on hpc3

---
+++ submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -J tors-mul-bench
+#SBATCH -p standard
+#SBATCH -t 144:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10000mb
+#SBATCH --account dmobley_lab
+#SBATCH --export ALL
+#SBATCH --mail-user=bwestbr1@uci.edu
+#SBATCH --constraint=fastscratch
+#SBATCH -o slurm.out
+#SBATCH -e slurm.err
+
+date
+hostname
+
+mamba activate bench-dev
+module load imagemagick
+
+make output/out.png
+
+date
