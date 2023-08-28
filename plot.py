afe533bcf84073d1d926838c380b999e40e6d393
Brent Westbrook <brentrwestbrook@gmail.com>
2023-08-28 09:33:05 -0400

    plot TM vs Sage TM

--- plot.py
+++ plot.py
@@ -1,3 +1,14 @@
-from main import plot_cdfs
+from main import plot
 
-plot_cdfs(".", ["full-opt-output", "industry-output"])
+# "TM" is the Sage 2.1.0 force field with the torsion multiplicity changes,
+# trained on the tm dataset
+#
+# "Sage TM" is the original Sage 2.1.0 force field trained on the tm dataset
+#
+# "Sage" is the original Sage 2.1.0 force field trained on the original dataset
+
+plot(
+    "/tmp",
+    ["output/industry/", "output/industry/sage"],
+    names=["TM", "Sage TM"],
+)
