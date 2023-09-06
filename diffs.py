4ab0f86966395f8216ff04019a54cf6224d01727
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-06 14:08:07 -0400

    factor out display and add a module comment

--- diffs.py
+++ diffs.py
@@ -1,16 +1,19 @@
+# printing the differences from debug as sets and seeing how they overlap
+
 from pprint import pprint
 
 from debug import inner
 
-eps = 10.0
+
+def display(label, s):
+    print(f"{label} = {len(s)}")
+    pprint(s)
+
+
+eps = 5.0
 tm = set(inner(eps, False, "TM"))
 sage_tm = set(inner(eps, False, "Sage TM"))
 
-print("Shared")
-pprint(tm & sage_tm)
-
-print("TM only")
-pprint((tm & sage_tm) ^ tm)
-
-print("Sage TM only")
-pprint((tm & sage_tm) ^ sage_tm)
+display("Shared", tm & sage_tm)
+display("TM only", (tm & sage_tm) ^ tm)
+display("Sage TM only", (tm & sage_tm) ^ sage_tm)
