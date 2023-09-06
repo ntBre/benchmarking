e4c3db07785d553f50322079f2512639281e7f92
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-06 13:22:59 -0400

    make main and click command

--- debug.py
+++ debug.py
@@ -5,6 +5,7 @@ import importlib
 import sys
 from collections import defaultdict
 
+import click
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -54,67 +55,73 @@ def plot(data, filename="debug.png", dpi=200):
     plt.savefig(filename, dpi=dpi)
 
 
-tm = pd.read_csv("output/industry/dde.csv").rename(
-    columns={"difference": "TM"}
-)
-sage_tm = pd.read_csv("output/industry/sage/dde.csv").rename(
-    columns={"difference": "Sage TM"}
-)
-sage_sage = pd.read_csv("output/industry/sage_sage/dde.csv").rename(
-    columns={"difference": "Sage"}
-)
+@click.command()
+@click.option("--eps", default=10.0)
+@click.option("--draw", default=False)
+def main(eps, draw):
+    tm = pd.read_csv("output/industry/dde.csv").rename(
+        columns={"difference": "TM"}
+    )
+    sage_tm = pd.read_csv("output/industry/sage/dde.csv").rename(
+        columns={"difference": "Sage TM"}
+    )
+    sage_sage = pd.read_csv("output/industry/sage_sage/dde.csv").rename(
+        columns={"difference": "Sage"}
+    )
 
-data = tm.merge(sage_tm).merge(sage_sage)
+    data = tm.merge(sage_tm).merge(sage_sage)
 
-# hilarious
-data = data.rename(columns={"Unnamed: 0": "Record ID"})
+    # hilarious
+    data = data.rename(columns={"Unnamed: 0": "Record ID"})
 
-# list of records with substantial disagreements - 63 of these for eps = 10.0
-eps = 10.0
-diffs = data[abs(data["Sage TM"] - data["Sage"]) > eps]
+    # list of records with substantial disagreements - 63 of these for eps =
+    # 10.0
+    diffs = data[abs(data["Sage TM"] - data["Sage"]) > eps]
 
-# some of these are actually better in Sage TM, so filter further by the ones
-# with larger magnitudes in the TM version - 36 for eps = 10.0
-our_bad = diffs[abs(diffs["Sage TM"]) > abs(diffs["Sage"])]
-print(our_bad)
+    # some of these are actually better in Sage TM, so filter further by the
+    # ones with larger magnitudes in the TM version - 36 for eps = 10.0
+    our_bad = diffs[abs(diffs["Sage TM"]) > abs(diffs["Sage"])]
+    print(our_bad)
 
-records = list(our_bad["Record ID"])
+    records = list(our_bad["Record ID"])
 
-print(f"{len(records)} bad records")
+    print(f"{len(records)} bad records")
 
-ds = OptimizationResultCollection.parse_file("datasets/industry.json")
-data = [v for value in ds.entries.values() for v in value]
+    ds = OptimizationResultCollection.parse_file("datasets/industry.json")
+    data = [v for value in ds.entries.values() for v in value]
 
-data = [rec for rec in data if int(rec.record_id) in records]
+    data = [rec for rec in data if int(rec.record_id) in records]
 
-# I added a label header, but unless I disable comments, it eats most of the
-# SMIRKS patterns. so instead of using comment functionality, skip the comment
-# row and disable comments
-new_smirks = np.loadtxt(
-    "../valence-fitting/01_generate-forcefield/new_smirks.dat",
-    dtype=str,
-    comments=None,
-    skiprows=1,
-)
+    # I added a label header, but unless I disable comments, it eats most of
+    # the SMIRKS patterns. so instead of using comment functionality, skip the
+    # comment row and disable comments
+    new_smirks = np.loadtxt(
+        "../valence-fitting/01_generate-forcefield/new_smirks.dat",
+        dtype=str,
+        comments=None,
+        skiprows=1,
+    )
 
-molecules = [
-    Molecule.from_mapped_smiles(r.cmiles, allow_undefined_stereo=True)
-    for r in tqdm(data, desc="Converting to molecules")
-]
+    molecules = [
+        Molecule.from_mapped_smiles(r.cmiles, allow_undefined_stereo=True)
+        for r in tqdm(data, desc="Converting to molecules")
+    ]
 
-do_plot = False
+    counts = defaultdict(int)
+    for i, mol in enumerate(molecules):
+        for s, smirks in enumerate(new_smirks):
+            if mol.chemical_environment_matches(smirks):
+                counts[i] = 1
+                if draw:
+                    print(f"drawing mol {i}-{s}")
+                    known_issues.draw_rdkit(
+                        mol, f"debug/mol{i}-{s}.png", smirks, max_matches=1
+                    )
+                else:
+                    break
 
-counts = defaultdict(int)
-for i, mol in enumerate(molecules):
-    for s, smirks in enumerate(new_smirks):
-        if mol.chemical_environment_matches(smirks):
-            counts[i] = 1
-            if do_plot:
-                print(f"drawing mol {i}-{s}")
-                known_issues.draw_rdkit(
-                    mol, f"debug/mol{i}-{s}.png", smirks, max_matches=1
-                )
-            else:
-                break
+    print(f"{sum(counts.values())} matching records")
 
-print(f"{sum(counts.values())} matching records")
+
+if __name__ == "__main__":
+    main()
