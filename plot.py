2901779b64d36aa969519973b766c3db4aba0840
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-18 09:11:03 -0400

    try to unify plots

--- plot.py
+++ plot.py
@@ -2,21 +2,40 @@ import subprocess
 
 from main import plot
 
-plot(
-    "/tmp",
-    [
-        "output/industry/esp-tors-10",
-        "output/industry/esp-full",
-        # "output/industry/tm",
-        # "output/industry/sage-tm",
-        # "output/industry/sage-sage",
-        "output/industry/sage-2.1.0",
-    ],
-    # names=["TM", "Sage TM", "My Sage", "Sage"],
-    names=["esp-tors-10", "esp-full", "Sage"],
-)
+
+def plot_tm():
+    plot(
+        "/tmp",
+        [
+            "output/industry/tm",
+            # "output/industry/sage-tm",
+            # "output/industry/sage-sage",
+            "output/industry/sage-2.1.0",
+        ],
+        names=[
+            #
+            "TM",
+            # "Sage TM",
+            # "My Sage",
+            "Sage",
+        ],
+    )
 
 
+def plot_espaloma():
+    plot(
+        "/tmp",
+        [
+            "output/industry/esp-tors-10",
+            "output/industry/esp-full",
+            "output/industry/sage-2.1.0",
+        ],
+        names=["esp-tors-10", "esp-full", "Sage"],
+    )
+
+
+plot_tm()
+# plot_espaloma()
 subprocess.run(
     [
         "montage",
