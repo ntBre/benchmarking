36d276b973e11e38f57325b26639b29c378037bc
Brent Westbrook <brentrwestbrook@gmail.com>
2023-10-10 10:12:51 -0400

    move hist.py into main and add from_file

--- hist.py
+++ hist.py
@@ -10,10 +10,16 @@ from tqdm import tqdm
 
 
 class Record:
-    def __init__(self):
-        self.bonds = set()
-        self.angles = set()
-        self.torsions = set()
+    def __init__(self, bonds=None, angles=None, torsions=None):
+        if bonds is None:
+            bonds = set()
+        if angles is None:
+            angles = set()
+        if torsions is None:
+            torsions = set()
+        self.bonds = bonds
+        self.angles = angles
+        self.torsions = torsions
 
     def __repr__(self):
         return (
@@ -28,31 +34,48 @@ class Record:
             torsions=list(self.torsions),
         )
 
+    def from_file(filename):
+        "Load a sequence of `Record`s from `filename`"
+        with open(filename) as inp:
+            d = json.load(inp)
+        ret = {}
+        for rec, data in d.items():
+            ret[rec] = Record(
+                set(data["bonds"]), set(data["angles"]), set(data["torsions"])
+            )
+        return ret
+
 
 logging.getLogger("openff").setLevel(logging.ERROR)
 
-ds = OptimizationResultCollection.parse_file("datasets/industry.json")
-data = [v for value in ds.entries.values() for v in value]
 
-pairs = (
-    (
-        r.record_id,
-        Molecule.from_mapped_smiles(r.cmiles, allow_undefined_stereo=True),
+def main():
+    ds = OptimizationResultCollection.parse_file("datasets/industry.json")
+    data = [v for value in ds.entries.values() for v in value]
+
+    pairs = (
+        (
+            r.record_id,
+            Molecule.from_mapped_smiles(r.cmiles, allow_undefined_stereo=True),
+        )
+        for r in data
     )
-    for r in data
-)
 
-ff = ForceField("forcefields/tm.offxml", allow_cosmetic_attributes=True)
+    ff = ForceField("forcefields/tm.offxml", allow_cosmetic_attributes=True)
 
-records = defaultdict(Record)
-for rec_id, mol in tqdm(pairs, total=len(data), desc="Processing"):
-    labels = ff.label_molecules(mol.to_topology())[0]
-    for bond in labels["Bonds"].values():
-        records[rec_id].bonds.add(bond.id)
-    for angle in labels["Angles"].values():
-        records[rec_id].angles.add(angle.id)
-    for torsion in labels["ProperTorsions"].values():
-        records[rec_id].torsions.add(torsion.id)
+    records = defaultdict(Record)
+    for rec_id, mol in tqdm(pairs, total=len(data), desc="Processing"):
+        labels = ff.label_molecules(mol.to_topology())[0]
+        for bond in labels["Bonds"].values():
+            records[rec_id].bonds.add(bond.id)
+        for angle in labels["Angles"].values():
+            records[rec_id].angles.add(angle.id)
+        for torsion in labels["ProperTorsions"].values():
+            records[rec_id].torsions.add(torsion.id)
 
-with open("hist.json", "w") as out:
-    json.dump(records, out, indent=2, default=Record.to_json)
+    with open("hist.json", "w") as out:
+        json.dump(records, out, indent=2, default=Record.to_json)
+
+
+if __name__ == "__main__":
+    main()
