9a2b220f9c7630b4026be512ef5262be49352482
Brent Westbrook <brentrwestbrook@gmail.com>
2023-10-11 07:02:15 -0400

    pair ids directly with values, add torsion output

--- change.py
+++ change.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 from openff.toolkit import ForceField
 
 
-def plot(ids, lens, title, filename):
+def plot(lens, title, filename):
     fig, ax = plt.subplots(figsize=(12, 3))
+    ids, lens = zip(*lens)
     ax.bar(ids, lens)
     ax.set_xticklabels(ids, rotation=270)
     ax.set_title(title)
@@ -28,12 +29,10 @@ def check(param, attrs):
     hs = start.get_parameter_handler(param)
     he = end.get_parameter_handler(param)
 
-    ids = []
     ret = defaultdict(list)
     for s in hs:
         if "parameterize" not in s._cosmetic_attribs:
             continue
-        ids.append(s.id)
         e = he.get_parameter(dict(id=s.id))[0]
         for attr in attrs:
             f = getattr(e, attr, None)
@@ -41,27 +40,27 @@ def check(param, attrs):
             if f is None or i is None:
                 continue
             assert f.units == i.units
-            ret[attr].append((f - i).magnitude)
+            ret[attr].append((s.id, (f - i).magnitude))
 
-    return ids, ret
+    return ret
 
 
 def check_bonds():
-    ids, vals = check("Bonds", ["k", "length"])
-    plot(ids, vals["k"], "Bond k", "change/bond-k.png")
-    plot(ids, vals["length"], "Bond Len", "change/bond-len.png")
+    vals = check("Bonds", ["k", "length"])
+    plot(vals["k"], "Bond k", "change/bond-k.png")
+    plot(vals["length"], "Bond Len", "change/bond-len.png")
 
 
 def check_angles():
-    ids, vals = check("Angles", ["k", "angle"])
-    plot(ids, vals["k"], "Angle k", "change/angle-k.png")
-    plot(ids, vals["angle"], "Angle", "change/angle.png")
+    vals = check("Angles", ["k", "angle"])
+    plot(vals["k"], "Angle k", "change/angle-k.png")
+    plot(vals["angle"], "Angle", "change/angle.png")
 
 
 def check_torsions():
-    ids, vals = check("ProperTorsions", ["k1", "k2", "k3", "k4", "k5", "k6"])
+    vals = check("ProperTorsions", ["k1", "k2", "k3", "k4", "k5", "k6"])
     for i in range(1, 7):
-        plot(ids, vals[f"k{i}"], f"Torsion k{i}", f"change/angle-k{i}.png")
+        plot(vals[f"k{i}"], f"Torsion k{i}", f"change/torsion-k{i}.png")
 
 
 if __name__ == "__main__":
