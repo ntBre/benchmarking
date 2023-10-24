0c461936e4acd7b6a8e4868e363a578de199ca14
Brent Westbrook <brentrwestbrook@gmail.com>
2023-10-24 12:28:40 -0400

    simplify some plotting code

--- plot.py
+++ plot.py
@@ -3,6 +3,12 @@ import subprocess
 from main import plot
 
 
+def plotter(ffs, names=None):
+    if names is None:
+        names = ffs
+    plot("/tmp", [f"output/industry/{ff}" for ff in ffs], names)
+
+
 def plot_tm():
     plot(
         "/tmp",
@@ -12,60 +18,45 @@ def plot_tm():
             # "output/industry/sage-sage",
             # "output/industry/my-sage-2.1.0",
             "output/industry/pavan-2.1.0",
-            "output/industry/pavan-2.1.0-repeat",
-            # "output/industry/sage-2.1.0",
+            "output/industry/pavan-repeat",
+            # "output/industry/pavan-2.1.0-repeat",
+            "output/industry/sage-2.1.0",
         ],
         names=[
             # "TM-TM",
             # "Sage-TM",
             # "Sage-Sage",
-            # "My Sage 2.1.0",  # my environment (-oe) on sage 2.1.0 input files
+            # "My Sage 2.1.0",  # my environment (-oe) on sage 2.1 input files
             "Pavan env",  # pavan's env with sage 2.1.0 input files
-            "Pavan env bench 2",  # repeated only the benchmark
-            # "Sage",
+            "Pavan repeat",  # pavan's env with sage 2.1.0 input files again
+            # "Pavan env bench 2",  # repeated only the benchmark
+            "Sage",
         ],
     )
 
 
 def plot_espaloma():
-    plot(
-        "/tmp",
-        [
-            # "output/industry/esp-tors-10",
-            "output/industry/esp-full",
-            "output/industry/sage-2.1.0",
-            "output/industry/espaloma",
-        ],
-        names=[
-            #
-            # "esp-tors-10",
-            "esp-full",
-            "Sage",
-            "Espaloma",
-        ],
-    )
+    plotter(["esp-full", "sage-2.1.0", "espaloma"])
 
 
 def plot_besmarts():
-    plot(
-        "/tmp",
-        [
-            "output/industry/besmarts-ba",
-            "output/industry/sage-2.1.0",
-            "output/industry/espaloma",
-        ],
-        names=[
-            #
-            "besmarts ba",
-            "Sage",
-            "Espaloma",
-        ],
-    )
+    plotter(["besmarts-ba", "sage-2.1.0", "espaloma"])
+
+
+def plot_msm():
+    plotter(["sage-2.1.0", "msm"])
+
+
+def sage_sage():
+    plotter(["sage-sage", "sage-sage-new", "my-sage-2.1.0", "sage-2.1.0"])
 
 
 plot_tm()
 # plot_espaloma()
 # plot_besmarts()
+# plot_msm()
+# sage_sage()
+
 subprocess.run(
     [
         "montage",
