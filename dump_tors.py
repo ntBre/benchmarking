e3a753add920b9962e9579572a953506706f164a
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-08 16:26:40 -0500

    pull out all parameters, not just torsions

--- dump_tors.py
+++ dump_tors.py
@@ -4,8 +4,18 @@ from openff.toolkit import ForceField
 
 ff = ForceField(argv[1], allow_cosmetic_attributes=True)
 
-h = ff.get_parameter_handler("ProperTorsions")
 
+h = ff.get_parameter_handler("Bonds")
+for t in h.parameters:
+    print(f"{t.id}-k,{t.k.magnitude}")
+    print(f"{t.id}-length,{t.length.magnitude}")
+
+h = ff.get_parameter_handler("Angles")
+for t in h.parameters:
+    print(f"{t.id}-k,{t.k.magnitude}")
+    print(f"{t.id}-angle,{t.angle.magnitude}")
+
+h = ff.get_parameter_handler("ProperTorsions")
 for t in h.parameters:
     for i in range(1, 7):
         k = f"k{i}"
--- sage_tors_diff.R
+++ sage_tors_diff.R
@@ -1,6 +1,8 @@
-v1 <- read.csv("dump/sage-2.1.0.csv", header = FALSE)
-v2 <- read.csv("dump/pavan-2.1.0.csv", header = FALSE)
-v3 <- read.csv("dump/pavan-repeat.csv", header = FALSE)
+options(scipen = 999)
+
+v1 <- read.csv("dump/sage-2.1.0.all.csv", header = FALSE)
+v2 <- read.csv("dump/pavan-2.1.0.all.csv", header = FALSE)
+v3 <- read.csv("dump/pavan-repeat.all.csv", header = FALSE)
 
 colnames(v1) <- c("pid", "v1")
 colnames(v2) <- c("pid", "v2")
@@ -11,11 +13,38 @@ full <- merge(merge(v1, v2, sort = FALSE), v3, sort = FALSE)
 # percent differences
 full$p12 <- 100 * (full$v1 - full$v2) / full$v1
 full$p13 <- 100 * (full$v1 - full$v3) / full$v1
-full$p23 <- 200 * (full$v2 - full$v3) / full$v2
+full$p23 <- 100 * (full$v2 - full$v3) / full$v2
+
+# regular differences
+full$d12 <- full$v1 - full$v2
+full$d13 <- full$v1 - full$v3
+full$d23 <- full$v2 - full$v3
 
 plot(full$p12, ylab = "%Diff")
 
 full[which.max(full$p12), ]
 
+full[order(abs(full$p12), decreasing = TRUE), ]
+
+full[order(abs(full$p12), decreasing = TRUE), 1:7]
+
+# [#1:1]-[*;r3:2]~;!@[*:3]
+
+## variance in parameter value vs coverage
+## - maybe find threshold for coverage
+
+## keep an eye on t165-t167 too, should they be at the end? or always zero
+
 full[order(abs(full$p12), decreasing = TRUE), ] |> head()
-full[order(abs(full$p13), decreasing = TRUE), ] |> head()
+full[order(abs(full$p12), decreasing = TRUE), ] |> head()
+
+full[order(abs(full$d13), decreasing = TRUE), ] |> head()
+full[order(abs(full$d13), decreasing = TRUE), ] |> head()
+
+bond_lengths <- full[grepl("-length", full$pid),]
+bond_k <- full[grepl("^b.*-k", full$pid),]
+
+angle_angle <- full[grepl("-angle", full$pid),]
+angle_k <- full[grepl("^a.*-k", full$pid),]
+
+torsions <- full[grepl("^t.*-k", full$pid),]
