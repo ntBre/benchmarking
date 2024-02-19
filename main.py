678c99f2b63db1581200abd6b7f1e5e9b52d40d6
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-19 12:00:21 -0500

    move cache tests out of main

--- main.py
+++ main.py
@@ -109,28 +109,6 @@ class CachedResultCollection:
         return ret
 
 
-def test_cached_results():
-    print("loading collection")
-    opt = OptimizationResultCollection.parse_file("datasets/small-opt.json")
-    print("building cache")
-    crc = CachedResultCollection.from_qcsubmit_collection(opt)
-    print("serializing to json")
-
-    json_file = "/tmp/crc.json"
-    with open(json_file, "w") as out:
-        out.write(crc.to_json(indent=2))
-
-    print("re-reading from JSON")
-    two = CachedResultCollection.from_json(json_file)
-
-    assert len(two.inner) == len(crc.inner)
-
-    # it would be nice to assert that the values are equal, but the round-trip
-    # probably isn't working precisely
-
-    # assert two == crc
-
-
 @click.command()
 @click.option("--forcefield", "-f", default="force-field.offxml")
 @click.option("--dataset", "-d", default="datasets/industry.json")
---
+++ tests/test_cache.py
@@ -0,0 +1,24 @@
+from main import CachedResultCollection
+from openff.qcsubmit.results import OptimizationResultCollection
+
+
+def test_cached_results():
+    print("loading collection")
+    opt = OptimizationResultCollection.parse_file("datasets/small-opt.json")
+    print("building cache")
+    crc = CachedResultCollection.from_qcsubmit_collection(opt)
+    print("serializing to json")
+
+    json_file = "/tmp/crc.json"
+    with open(json_file, "w") as out:
+        out.write(crc.to_json(indent=2))
+
+    print("re-reading from JSON")
+    two = CachedResultCollection.from_json(json_file)
+
+    assert len(two.inner) == len(crc.inner)
+
+    # it would be nice to assert that the values are equal, but the round-trip
+    # probably isn't working precisely
+
+    # assert two == crc
