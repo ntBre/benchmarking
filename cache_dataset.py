0af16d0d3cb6b52fc7df056fd1b65bc02659bbb0
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-19 13:32:42 -0500

    add some status prints

--- cache_dataset.py
+++ cache_dataset.py
@@ -12,9 +12,12 @@ logging.getLogger("openff").setLevel(logging.ERROR)
 @click.option("--dataset", "-d", default="datasets/industry.json")
 @click.option("--output", "-o", default="datasets/cache/industry.json")
 def main(dataset, output):
+    print("loading collection")
     opt = OptimizationResultCollection.parse_file(dataset)
+    print("caching records")
     crc = CachedResultCollection.from_qcsubmit_collection(opt)
 
+    print(f"serializing to json in {output}")
     with open(output, "w") as out:
         out.write(crc.to_json(indent=2))
 
