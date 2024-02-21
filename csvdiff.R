931b632eb5983d54d9171e89c2e12d3a8318a646
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-21 12:43:00 -0500

    improve default and debug printing

--- csvdiff.R
+++ csvdiff.R
@@ -1,3 +1,5 @@
+debug <- Sys.getenv("RDEBUG") == 1
+
 args <- commandArgs(trailingOnly = TRUE)
 csv1 <- read.csv(args[1], header = TRUE)
 csv2 <- read.csv(args[2], header = TRUE)
@@ -5,6 +7,8 @@ csv2 <- read.csv(args[2], header = TRUE)
 both <- merge(csv1, csv2, by = "X")
 colnames(both) <- c("id", "v1", "v2")
 
-print("maximum difference:")
-max(both$v1 - both$v2)
-both[which.max(both$v1 - both$v2), ]
+print(sprintf("maximum difference: %f", max(both$v1 - both$v2)))
+
+if (debug) {
+    both[which.max(both$v1 - both$v2), ]
+}
