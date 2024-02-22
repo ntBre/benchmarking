d25af2da3d61336f9219a74d2ee6b76bc63b664b
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-22 14:56:19 -0500

    get more creative with RDEBUG

--- csvdiff.R
+++ csvdiff.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-debug <- Sys.getenv("RDEBUG") == 1
+debug <- Sys.getenv("RDEBUG")
 
 args <- commandArgs(trailingOnly = TRUE)
 csv1 <- read.csv(args[1], header = TRUE)
@@ -11,8 +11,12 @@ stopifnot(length(csv1$X) == length(csv2$X))
 both <- merge(csv1, csv2, by = "X")
 colnames(both) <- c("id", "v1", "v2")
 
-print(sprintf("maximum difference: %f", max(both$v1 - both$v2)))
+diff <- both$v1 - both$v2
+print(sprintf("maximum difference: %f, mean: %f", max(diff), mean(diff)))
 
-if (debug) {
-    both[which.max(both$v1 - both$v2), ]
+if (debug == 1) {
+    both[which.max(diff), ]
+} else if (debug > 1) {
+    both$diff <- diff
+    head(both[order(abs(diff), decreasing = TRUE), ], n = as.integer(debug))
 }
