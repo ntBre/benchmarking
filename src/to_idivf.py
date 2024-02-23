ec94e48c0d12ba44b4a1c0ec6a3a6e5769b64fda
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-23 13:58:36 -0500

    prepare to generate idivf auto only for TM params
    this is the full list of parameters marked by Pavan and Meghan as incorrect, as
    taken from my new TM to_add.dat file

--- src/to_idivf.py
+++ src/to_idivf.py
@@ -3,8 +3,13 @@ from openff.toolkit import ForceField
 init = ForceField("openff-2.1.0.offxml")
 h = init.get_parameter_handler("ProperTorsions")
 
+with open("new_tm_params.dat") as inp:
+    new_params = [line.strip() for line in inp]
+
 for p in h.parameters:
+    if p.id not in new_params:
+        continue
     for per in range(len(p.periodicity)):
         p.idivf[per] = "experimental-auto"
 
-init.to_file("idivf-sage-2.1.0.offxml")
+init.to_file("forcefields/tm-idivf-sage-2.1.0.offxml")
