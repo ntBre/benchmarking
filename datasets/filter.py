ef09c40a235f2576a4238bf9294cdfbf62f78a6e
Brent Westbrook <brentrwestbrook@gmail.com>
2024-05-07 18:50:16 -0400

    also check for unassigned torsions, filter out 1 additional record

--- datasets/filter.py
+++ datasets/filter.py
@@ -1,7 +1,11 @@
 import logging
 
+import qcportal  # for zstd issue
+from openff.interchange import Interchange
+from openff.interchange.exceptions import UnassignedTorsionError
 from openff.qcsubmit.results import OptimizationResultCollection
 from openff.qcsubmit.results.filters import SinglepointRecordFilter
+from openff.toolkit import ForceField
 from openff.toolkit.utils.exceptions import (
     ChargeCalculationError,
     ConformerGenerationError,
@@ -23,10 +27,24 @@ class ChargeCheckFilter(SinglepointRecordFilter):
             return True
 
 
+class UnassignedTorsionFilter(SinglepointRecordFilter):
+    def _filter_function(self, result, record, molecule) -> bool:
+        try:
+            Interchange.from_smirnoff(
+                ForceField("openff-2.1.0.offxml"), [molecule]
+            )
+        except UnassignedTorsionError:
+            return False
+        else:
+            return True
+
+
 dataset = OptimizationResultCollection.parse_file("unfiltered-supp.json")
 print("before: ", dataset.n_results)
 dataset = dataset.filter(ChargeCheckFilter())
-print("after: ", dataset.n_results)
+print("after charge: ", dataset.n_results)
+dataset = dataset.filter(UnassignedTorsionFilter())
+print("after torsions: ", dataset.n_results)
 
 with open("supp.json", "w") as out:
     out.write(dataset.json(indent=2))
