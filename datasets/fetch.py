22704666225877cb5047d7a9149f111d3afcfcb2
Brent Westbrook <brentrwestbrook@gmail.com>
2024-05-07 17:43:12 -0400

    add script for fetching new dataset and output

---
+++ datasets/fetch.py
@@ -0,0 +1,12 @@
+from openff.qcsubmit.results import OptimizationResultCollection
+from qcportal import PortalClient
+
+client = PortalClient("https://api.qcarchive.molssi.org:443/")
+dataset = OptimizationResultCollection.from_server(
+    client=client,
+    datasets=["OpenFF Torsion Benchmark Supplement v1.0"],
+    spec_name="default",
+)
+
+with open("supp.json", "w") as out:
+    out.write(dataset.json(indent=2))
